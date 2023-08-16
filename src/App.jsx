6d1b59ee8dd42577e7e667aca4a948d8583353aa
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-08-16 16:27:17 -0400

    display shows correct numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
+import { useState } from 'react';
 import { Container } from '@mui/material';
 import Header from './components/Header/Header';
 import Display from './components/Calculator/Display';
-import { useState } from 'react';
 import CalculatorBody from './components/Calculator/CalculatorBody';
 
 function App() {
 
-  const [inputDisplay, setInputDisplay] = useState(1156);
-  console.log(inputDisplay)
+  const [final, setFinal] = useState('');
+  const [currentNumbers, setCurrentNumbers] = useState('');
+  const [currentOperator, setCurrentOperator] = useState('');
+  const [prevNumbers, setPrevNumbers] = useState('');
+
   return (
 
       <Container maxWidth={false} sx={{ height: "max-content", pb: '2rem', backgroundColor: "veryDarkDesaturatedBlue"}} >
         <Header />
-        <Display inputDisplay={inputDisplay}/>
-        <CalculatorBody />
+        <Display final={final} currentNumbers={currentNumbers} currentOperator={currentOperator} prevNumbers={prevNumbers} />
+        <CalculatorBody final={final} setFinal={setFinal} currentNumbers={currentNumbers} setCurrentNumbers={setCurrentNumbers} currentOperator={currentOperator} setCurrentOperator={setCurrentOperator} prevNumbers={prevNumbers} setPrevNumbers={setPrevNumbers} />
       </Container>
   );
 }
--- src/components/Calculator/Display.jsx
+++ src/components/Calculator/Display.jsx
@@ -1,11 +1,21 @@
 import { Box, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
 
-const Display = ({inputDisplay}) => {
-console.log(inputDisplay)
+const Display = ({final, currentNumbers, currentOperator, prevNumbers}) => {
+const [numDisplay, setNumDisplay] = useState('0');  
+let choice;
+useEffect(() =>{
+ choice = currentNumbers ? currentNumbers : final
+ if(isNaN(choice) || choice === '') choice = prevNumbers
+ if(isNaN(choice) || choice === '') choice = '0';
+ setNumDisplay(choice);
 
-    return (
+ }, [currentNumbers, final, prevNumbers])
+
+    
+return (
     <Box sx={{backgroundColor: 'veryDarkDesaturatedBlue3', borderRadius: '10px', height: '6rem', display: 'flex', alignItems: 'center', justifyContent: 'end', color: 'white', mt: "2rem", p: '2rem'}}>
-        <Typography variant="h1" component="h1" sx={{color: 'white', alignSelf: "baseline"}}>{inputDisplay.toLocaleString('en')}</Typography>
+        <Typography variant="h1" component="h1" sx={{color: 'white', alignSelf: "baseline"}}>{parseFloat(numDisplay).toLocaleString('en')}{' '}{currentOperator}</Typography>
     </Box>
   )
 }
